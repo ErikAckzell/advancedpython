0899068034b481a4247311ed39c96fcd43be3e2e
Erik Ackzell <erik.ackzell@gmail.com>
2016-09-23 08:25:39 +0200

    Minor fixes, added documentation.

--- homework2/QuasiNewtonMethods.py
+++ homework2/QuasiNewtonMethods.py
@@ -14,22 +14,41 @@ import unittest
 
 
 class QuasiNewtonMethods:
+    """
+    This is a general class for Quasi Newton methods. It contains methods
+    which are the same for all Quasi Newton methods.
+    """
 
-    def __init__(self, problem, dim):
-        self.x = scipy.zeros(dim)
-        self.x_old = scipy.ones(dim)
+    def __init__(self, problem, x0):
+        """
+        An object of the class is initialized by an object of the
+        optimizationProblem class problem, and an initial guess for the
+        solution x0.
+        """
+        self.x = scipy.zeros_like(x0)
+        self.x_old = x0
         self.f = problem.f
         self.g = problem.g
         self.gk = self.g(self.x)
-        self.gk_old = self.g(self.x_old)
-        self.H = scipy.eye(dim)
+        self.gk_old = self.g(x0)
+        self.H = scipy.eye(len(x0))
 
-    def get_solution(self, tolerance=1e-8):
-        while scipy.linalg.norm(self.g(self.x)) > tolerance:
+    def get_solution(self, tolerance=1e-8, maxiter=200):
+        """
+        This method calculates and returns the solution x when g(x) < tolerance
+        or stops the calculation when a maximum number of iterations has been
+        performed.
+        """
+        i = 1
+        while scipy.linalg.norm(self.g(self.x)) >= tolerance and i <= maxiter:
             self.perform_iteration()
+            i += 1
         return self.x
 
     def perform_iteration(self):
+        """
+        This method performs one iteration of the Quasi Newton method.
+        """
         sk = self.get_sk()
         alphak = self.get_alphak()
         self.update_x(sk=sk, alphak=alphak)
@@ -37,42 +56,83 @@ class QuasiNewtonMethods:
 
 class NewtonMethod(QuasiNewtonBaseClass.QuasiNewtonBase,
                    QuasiNewtonMethods):
-
+    """
+    This is a class for the classic Newton method. It inherits from both
+    the QuasiNewtonMethods class and the abstract base class
+    QuasiNewtonBase
+    """
     def update_x(self, sk, alphak):
+        """
+        This mehod updates the solution x, given an s and an alpha.
+        """
         self.x_old = scipy.copy(self.x)
         self.x = self.x_old + alphak * sk
 
     def update_H(self):
+        """
+        This method updates H, which is an approximation of the inverse of the
+        Hessian of the objective function.
+        """
         pass
 
     def get_alphak(self):
+        """
+        This method calculates and returns the current alpha.
+        """
         pass
 
     def get_sk(self):
+        """
+        This method calculates and returns the current s.
+        """
         return - self.H.dot(self.g(self.x))
-
-############# SECANT METHODS (TASK 9) #############
 
 
 class SecantMethodUpdate(QuasiNewtonMethods):
+    """
+    This is a general class for the Quasi Newton methods which uses a secant
+    method. It inherits from the QuasiNewtonMethods class.
+    """
 
     def get_delta(self):
+        """
+        This method calculates and returns the current delta, which is the
+        difference of the old and the current solution guess.
+        """
         return self.x - self.x_old
 
     def get_gamma(self):
+        """
+        This method calculates and returns the current gamma, which is the
+        difference of the gradient evaluated in the old and the current
+        solution guess.
+        """
         return self.gk - self.gk_old
 
     def update_x(self):
+        """
+        This method updates the solution guess.
+        """
         self.x_old = scipy.copy(self.x)
         self.x = self.x - self.H.dot(self.gk)
 
 
 class goodBroyden(SecantMethodUpdate):
-
+    """
+    This is a class for the "good" Broyden method using the Sherman Morrison
+    update for approximating the inverse of the Hessian of the objective
+    function. It inherents from the SecantMethodUpdate class.
+    """
     def update_H(self):
+        """
+        This method updates H.
+        """
         self.shermanMorrisonUpdate()
 
     def shermanMorrisonUpdate(self):
+        """
+        This method implements the "good" Broyden method.
+        """
         gamma = self.get_gamma()
         delta = self.get_delta()
         u = delta - self.H.dot(gamma)
@@ -81,11 +141,21 @@ class goodBroyden(SecantMethodUpdate):
 
 
 class badBroyden(SecantMethodUpdate):
+    """
+    This is a class for the "bad" Broyden method. It inherits from the
+    SecantMethodUpdate class.
+    """
 
     def update_H(self):
+        """
+        This method updates H.
+        """
         self.bad_update()
 
     def bad_update(self):
+        """
+        This method implements the "bad" Broyden method.
+        """
         gamma = self.get_gamma()
         delta = self.get_delta()
         self.H = (self.H +
@@ -95,11 +165,21 @@ class badBroyden(SecantMethodUpdate):
 
 
 class DFP(SecantMethodUpdate):
+    """
+    This is a class for the DFP method. It inherits from the
+    SecantMethodUpdate class.
+    """
 
     def update_H(self):
-        pass
+        """
+        This method updates H.
+        """
+        self.DFPupdate()
 
     def DFPupdate(self):
+        """
+        This method implements the DFP method.
+        """
         gamma = self.get_gamma()
         delta = self.get_delta()
         summand2 = delta.dot(delta.transpose()) / delta.transpose().dot(gamma)
@@ -109,8 +189,15 @@ class DFP(SecantMethodUpdate):
 
 
 class BFGS(SecantMethodUpdate):
+    """
+    This is a class for the BFGS method. It inherits from the
+    SecantMethodUpdate class.
+    """
 
     def update_H(self):
+        """
+        This method updates H.
+        """
         gamma = self.get_gamma()
         delta = self.get_delta()
         summand2 = ((1 + (gamma.transpose().dot(self.H).dot(gamma) /
@@ -141,4 +228,14 @@ class TestOptimization(unittest.TestCase):
                                        instance.get_solution(1e-8))
 
 if __name__ == '__main__':
-#    unittest.main()
+    # unittest.main()
+    def f(x): return x[0] + x[1]
+
+    def g(x): return scipy.array([1, 1])
+
+    p = optimizationProblemclass.optimizationProblem(function=f, gradient=g)
+    instance = NewtonMethod(p, 2)
+    instance = goodBroyden(p, 2)
+    instance = badBroyden(p, 2)
+    instance = DFP(p, 2)
+    instance = BFGS(p, 2)
