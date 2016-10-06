2df6fc81605629d2bf0ef1a03036cd90fdbf0b91
Erik Ackzell <erik.ackzell@gmail.com>
2016-10-06 18:02:51 +0200

    Working on a plotting method for room 2

--- homework3/Room2Class.py
+++ homework3/Room2Class.py
@@ -7,7 +7,8 @@ Created on Thu Oct  6 10:41:43 2016
 
 
 import scipy
-
+from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D
 
 class room2:
     """
@@ -21,44 +22,56 @@ class room2:
             raise ValueError('westwall and eastwall must have same length')
         if not len(northwall) == len(southwall):
             raise ValueError('northwall and southwall must have same length')
-        self.u = scipy.zeros((len(westwall), len(northwall)))
-        self.u[:, 0] = westwall
-        self.u[0, :] = northwall
-        self.u[:, -1] = eastwall
-        self.u[-1, :] = southwall
+        self.umatrix = scipy.zeros((len(westwall), len(northwall)))
+        self.umatrix[:, 0] = westwall
+        self.umatrix[0, :] = northwall
+        self.umatrix[:, -1] = eastwall
+        self.umatrix[-1, :] = southwall
         self.h = h
         self.matrix = self.setup_matrix()
         self.rhs = self.setup_rhs()
+        self.u = self.umatrix[1:-1, 1:-1].flatten()
 
     def setup_matrix(self):
         column = scipy.concatenate((scipy.array([-4, 1]),
-                                    scipy.zeros(len(self.u[0]) - 4)))
+                                    scipy.zeros(len(self.umatrix[0]) - 4)))
         T = scipy.linalg.toeplitz(column)
-        Ttuple = tuple((T for _ in range(len(self.u[:, 0]) - 2)))
+        Ttuple = tuple((T for _ in range(len(self.umatrix[:, 0]) - 2)))
         A = scipy.linalg.block_diag(*Ttuple) + \
-            scipy.eye((len(self.u[0]) - 2) * (len(self.u[:, 0]) - 2),
-                      k=len(self.u[0]) - 2) + \
-            scipy.eye((len(self.u[0]) - 2) * (len(self.u[:, 0]) - 2),
-                      k=-(len(self.u[0]) - 2))
+            scipy.eye((len(self.umatrix[0]) - 2) * (len(self.umatrix[:, 0]) - 2),
+                      k=len(self.umatrix[0]) - 2) + \
+            scipy.eye((len(self.umatrix[0]) - 2) * (len(self.umatrix[:, 0]) - 2),
+                      k=-(len(self.umatrix[0]) - 2))
         return A
 
     def setup_rhs(self):
-        N = len(self.u[0])
-        M = len(self.u[:, 0])
+        N = len(self.umatrix[0])
+        M = len(self.umatrix[:, 0])
         rhs = scipy.zeros(((M - 2), (N - 2)))
-        rhs[0, 0] = self.u[0, 1] + self.u[1, 0]
-        rhs[0, -1] = self.u[0, N-2] + self.u[1, N-1]
-        rhs[-1, 0] = self.u[M-2, 0] + self.u[M-1, 1]
-        rhs[-1, -1] = self.u[M-1, N-2] + self.u[M-2, N-1]
-        print(rhs)
+        rhs[0, 0] = self.umatrix[0, 1] + self.umatrix[1, 0]
+        rhs[0, -1] = self.umatrix[0, N-2] + self.umatrix[1, N-1]
+        rhs[-1, 0] = self.umatrix[M-2, 0] + self.umatrix[M-1, 1]
+        rhs[-1, -1] = self.umatrix[M-1, N-2] + self.umatrix[M-2, N-1]
         for i in range(1, N-3):
-            rhs[0, i] = self.u[0, i]
-            rhs[-1, i] = self.u[M-1, i]
-        print(rhs)
+            rhs[0, i] = self.umatrix[0, i]
+            rhs[-1, i] = self.umatrix[M-1, i]
         for i in range(1, M-3):
-            rhs[i, 0] = self.u[i, 0]
-            rhs[i, -1] = self.u[i, N-1]
+            rhs[i, 0] = self.umatrix[i, 0]
+            rhs[i, -1] = self.umatrix[i, N-1]
         return (- (1 / self.h ** 2) * rhs).flatten()
+
+    def get_solution(self):
+        self.u = scipy.linalg.solve(self.matrix, self.rhs)
+
+    def plot(self):
+        self.umatrix[1:-1, 1:-1] = self.u.reshape()
+        figure = pyplot.figure()
+        axis = pyplot.subplot(111, projection='3d')
+        x = scipy.arange(0, self.h * len(self.umatrix[0]), self.h)
+        y = scipy.arange(0, self.h * len(self.umatrix[:, 0]), self.h)
+        X, Y = scipy.meshgrid(x, y)
+        Axes3D.plot_wireframe(X, Y, self.u)
+
 
 if __name__ == '__main__':
     h = 0.5
@@ -69,6 +82,6 @@ if __name__ == '__main__':
               eastwall=westwall,
               southwall=northwall,
               h=h)
-    print(R.matrix)
+#    print(R.matrix)
     print(R.rhs)
-    print(R.u)
+#    print(R.u)
